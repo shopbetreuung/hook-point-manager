f12e76f911c9ac83a0d5cf13ad646d55dd8ad147
Robin Wieschendorf <mail@robinwieschendorf.de>
2020-07-14 22:26:28 +0200

    docs: add documentation

--- new_files/vendor-no-composer/robinthehood/HookPointManager/Classes/DefaultHookPoints/DefaultHookPointsFor2051.php
+++ new_files/vendor-no-composer/robinthehood/HookPointManager/Classes/DefaultHookPoints/DefaultHookPointsFor2051.php
@@ -1,6 +1,20 @@
 <?php
 namespace RobinTheHood\HookPointManager\Classes\DefaultHookPonts;
 
+/*
+    *** Default Hook Points for Modified 2.0.5.1 ***
+    You can add new hook points by making a pull request in https://github.com/RobinTheHood/hook-point-manager
+
+    index   | description                                           | example value
+    --------------------------------------------------------------------------------------------------
+    name    | uniqu name of the hook point                          | hpm-default-create-account-begin
+    module  | module name of hook poit creator                      | robinthehood/hook-point-manager
+    file    | file path in which the hook point is to be installed  | /create_account.php
+    hash    | md5-Hash of orignal unmodified file                   | 2b5ce65ba6177ed24c805609b28572a7
+    line    | line after which the hook point is to be installed    | 30
+    include | auto_include directory for the hook point files       | /includes/extras/create_account/begin/
+ */
+
 class DefaultHookPointsFor2051
 {
     public function registerAll()
@@ -10,12 +24,12 @@ class DefaultHookPointsFor2051
         $hookPointManager = new HookPointManager();
 
         $hookPointManager->registerHookPoint([
-            'name' => 'hpm-default-create-account-1',
-            'module' => 'robinthehood/my-first-module',
+            'name' => 'hpm-default-create-account-begin',
+            'module' => 'robinthehood/hook-point-manager',
             'file' => '/create_account.php',
-            'fileHash' => '// Enter md5-Hash of orignal unmodified file',
+            'hash' => '2b5ce65ba6177ed24c805609b28572a7',
             'line' => 30,
-            'include' => '/includes/extras/create_account/befor/'
+            'include' => '/includes/extras/create_account/begin/'
         ], $modifiedVersions);
 
     }
